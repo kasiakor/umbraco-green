11561a008c5cd71c73854c46a94c9ff5b353e14d
kasia.kordys <kasia.kordys@gmail.com>
2020-06-02 21:36:22 +0100

    add site layout controller

---
+++ NewGood/SiteLayoutController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace NewGood
+{
+    public class SiteLayoutController  :  SurfaceController
+    {
+        public ActionResult RenderHeader()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/_Header.cshtml");
+        }
+    }
+}
