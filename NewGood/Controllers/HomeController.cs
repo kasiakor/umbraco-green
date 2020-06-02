2fb14abefbe9262714f38466be875b2d4ab4b409
kasia.kordys <kasia.kordys@gmail.com>
2020-06-02 22:24:54 +0100

    add home controller

---
+++ NewGood/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace NewGood.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private ActionResult RenderFeatured()
+        {
+            return PartialView("~/Views/Partials/Home/_Featured.cshtml");
+        }
+    }
+}
