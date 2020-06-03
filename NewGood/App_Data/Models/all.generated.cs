bbccdc852c8c94bf6fbee19ad9dc124bbcf8c37a
kasia.kordys <kasia.kordys@gmail.com>
2020-06-03 20:52:45 +0100

    add links to pages

--- NewGood/App_Data/Models/all.generated.cs
+++ NewGood/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "e16d4889ea31c053")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
