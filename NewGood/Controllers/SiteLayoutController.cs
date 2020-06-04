b10874fd7a7776071c6f83ade684c331b649adde
kasia.kordys <kasia.kordys@gmail.com>
2020-06-04 14:16:43 +0100

    add controller methods for navigation items

--- NewGood/Controllers/SiteLayoutController.cs
+++ NewGood/Controllers/SiteLayoutController.cs
@@ -1,13 +1,63 @@
-﻿using System.Web.Mvc;
+﻿using NewGood.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
+using System.Linq;
 
 namespace NewGood
 {
     public class SiteLayoutController : SurfaceController
     {
+        /// <summary>
+        /// Renders the header partial with navigation
+        /// </summary>
+        /// <returns>Partial view with a model</returns>
+
         public ActionResult RenderHeader()
         {
+            // when renderheader is called it calls GetNavigationModelFromDatabase() method
+            List<NavigationListItem> nav = GetNavigationModelFromDatabase();
             return PartialView("~/Views/Partials/SiteLayout/_Header.cshtml");
+        }
+
+        /// <summary>
+        /// Finds the home page and gets the navigation structure based on it and it's children
+        /// </summary>
+        /// <returns>A List of NavigationListItems, representing the structure of the site.</returns>
+        private List<NavigationListItem> GetNavigationModelFromDatabase()
+        {
+            //const int HOME_PAGE_POSITION_IN_PATH = 1;
+            //int homePageId = int.Parse(CurrentPage.Path.Split(',')[HOME_PAGE_POSITION_IN_PATH]);
+            //IPublishedContent homePage = Umbraco.Content(homePageId);
+            IPublishedContent homePage = CurrentPage.AncestorOrSelf(1).DescendantsOrSelf().Where(x => x.DocumentTypeAlias == "home").FirstOrDefault();
+            List<NavigationListItem> nav = new List<NavigationListItem>();
+            nav.Add(new NavigationListItem(new NavigationLink(homePage.Url, homePage.Name)));
+            nav.AddRange(GetChildNavigationList(homePage));
+            return nav;
+        }
+
+        /// <summary>
+        /// Loops through the child pages of a given page and their children to get the structure of the site.
+        /// </summary>
+        /// <param name="page">The parent page which you want the child structure for</param>
+        /// <returns>A List of NavigationListItems, representing the structure of the pages below a page.</returns>
+        private List<NavigationListItem> GetChildNavigationList(IPublishedContent page)
+        {
+            List<NavigationListItem> listItems = null;
+            var childPages = page.Children.Where("Visible");
+            if (childPages != null && childPages.Any() && childPages.Count() > 0)
+            {
+                listItems = new List<NavigationListItem>();
+                foreach (var childPage in childPages)
+                {
+                    NavigationListItem listItem = new NavigationListItem(new NavigationLink(childPage.Url, childPage.Name));
+                    listItem.Items = GetChildNavigationList(childPage);
+                    listItems.Add(listItem);
+                }
+            }
+            return listItems;
         }
 
         public ActionResult RenderFooter()
