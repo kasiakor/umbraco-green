bcc6b55dc2149f0aea4cf68b21862517cebaf728
kasia.kordys <kasia.kordys@gmail.com>
2020-06-08 20:31:31 +0100

    add controller for featured items

--- NewGood/Controllers/HomeController.cs
+++ NewGood/Controllers/HomeController.cs
@@ -1,5 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using NewGood.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
+using System.Linq;
+using Archetype;
+using Archetype.Models;
 
 namespace NewGood.Controllers
 {
@@ -17,7 +24,35 @@ namespace NewGood.Controllers
 
         public ActionResult RenderFeatured()
         {
-            return PartialView("~/Views/Partials/Home/_Featured.cshtml");
+           List<FeaturedItem> model = new List<FeaturedItem> ();
+            IPublishedContent homePage = CurrentPage.AncestorOrSelf(1).DescendantsOrSelf().Where(x => x.DocumentTypeAlias == "home").FirstOrDefault();
+
+            //Get property from the homepage
+            //From ArchetypeMode model get all the fieldsets and put them into FeaturedItem class
+            ArchetypeModel featuredItems = homePage.GetPropertyValue<ArchetypeModel>("featuredItems");
+
+            foreach(ArchetypeFieldsetModel fieldset in featuredItems)
+            {
+
+                string name = fieldset.GetValue<string>("name");
+                string category = fieldset.GetValue<string>("category");
+
+                //imageUrl
+                int imageId = fieldset.GetValue<int>("image");
+                //gets the value of media picker
+                var mediaItem = Umbraco.Media(imageId);
+                string imageUrl = mediaItem.Url;
+
+
+                //linkUrl
+                int pageId = fieldset.GetValue<int>("page");
+                IPublishedContent page = Umbraco.TypedContent(pageId);
+                string linkUrl = page.Url;
+
+
+                model.Add(new FeaturedItem(name, category, imageUrl, linkUrl));
+            }
+            return PartialView("~/Views/Partials/Home/_Featured.cshtml", model);
         }
 
         public ActionResult RenderBlog()
