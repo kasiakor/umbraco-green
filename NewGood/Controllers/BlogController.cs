666fd373ee7896df861abff80c164a7ea8a2fde8
kasia.kordys <kasia.kordys@gmail.com>
2020-06-10 22:12:04 +0100

    add blog controller

--- NewGood/Controllers/BlogController.cs
+++ NewGood/Controllers/BlogController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NewGood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
 
 namespace NewGood.Controllers
@@ -11,7 +14,30 @@ namespace NewGood.Controllers
     {
         public ActionResult RenderPostList()
         {
-            return PartialView("~/Views/Partials/Blog/_PostList.cshtml");
+            List<BlogPostList> model = new List<BlogPostList>();
+            IPublishedContent blogPage = CurrentPage.AncestorOrSelf(1).DescendantsOrSelf().Where(x => x.DocumentTypeAlias == "blog").FirstOrDefault();
+
+            foreach (IPublishedContent page in blogPage.Children.OrderByDescending(x => x.UpdateDate))
+            {
+
+                //string name = page.GetPropertyValue<string>("title") or:
+
+                string name = page.Name;
+                string introduction = page.GetPropertyValue<string>("articleIntro");
+
+                //imageUrl
+                int imageId = page.GetPropertyValue<int>("articleImage");
+                //gets the value of media picker
+                var mediaItem = Umbraco.Media(imageId);
+                string imageUrl = mediaItem.Url;
+
+                //linkUrl
+                string linkUrl = page.Url;
+
+
+                model.Add(new BlogPostList(name, introduction, imageUrl, linkUrl));
+            }
+            return PartialView("~/Views/Partials/Blog/_PostList.cshtml", model);
         }
     }
 }
