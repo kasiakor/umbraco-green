0892d83d98048e56c2f1cd9ba58976d1f4c9d027
kasia.kordys <kasia.kordys@gmail.com>
2020-06-10 18:25:51 +0100

    add model controller and view for blog post list

---
+++ NewGood/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace NewGood.Controllers
+{
+    public class BlogController : SurfaceController
+    {
+        public ActionResult RenderPostList()
+        {
+            return PartialView("~/Views/Partials/Blog/_PostList.cshtml");
+        }
+    }
+}
---
+++ NewGood/Models/BlogPostList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewGood.Models
+{
+    public class BlogPostList
+    {
+        public string Name { get; set; }
+        public string Introduction { get; set; }
+        public string ImageUrl { get; set; }
+        public string LinkUrl { get; set; }
+
+        public BlogPostList(string name, string introduction, string imageUrl, string linkUrl)
+        {
+            Name = name;
+            Introduction = introduction;
+            ImageUrl = imageUrl;
+            LinkUrl = linkUrl;
+        }
+    }
+}
