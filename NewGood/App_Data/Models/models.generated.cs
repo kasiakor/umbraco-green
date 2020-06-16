90447bd1f3004b695740064c3c3f5759c548c498
kasia.kordys <kasia.kordys@gmail.com>
2020-06-16 17:54:29 +0100

    add testimonial model

--- NewGood/App_Data/Models/models.generated.cs
+++ NewGood/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "d2c5262d23ca82ec")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
